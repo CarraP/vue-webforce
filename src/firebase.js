44988d8a3d42739e86fd9687a5a5f5fbc581d55d
thibaud-g <thibaud.gippet@gmail.com>
2023-03-15 14:05:58 +0100

    fix(firebase): update package

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
 import firebase from "firebase/app";
-import "firebase/firestore";
 import "firebase/auth";
+import "firebase/firestore";
 
 import { ref, onUnmounted, computed } from "vue";
 
 firebase.initializeApp({
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: "AIzaSyD13kcP3ekX9bg6f2uFHA1X4xOJoTKDsqc",
+  authDomain: "webforce3-3.firebaseapp.com",
+  databaseURL:
+    "https://webforce3-3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "webforce3-3",
+  storageBucket: "webforce3-3.appspot.com",
+  messagingSenderId: "253342471147",
+  appId: "1:253342471147:web:7038d20e2797392a4f7fae",
+  measurementId: "G-6607L3KEJH",
 });
+
+const auth = firebase.auth();
 
 export function useAuth() {
   const user = ref(null);
@@ -25,40 +28,36 @@ export function useAuth() {
     const googleProvider = new firebase.auth.GoogleAuthProvider();
     await auth.signInWithPopup(googleProvider);
   };
-
   const signOut = () => auth.signOut();
 
   return { user, isLogin, signIn, signOut };
 }
 
 const firestore = firebase.firestore();
-
-const messagesColleciton = firestore.collection("messages");
-const messagesQuery = messagesColleciton
+const messagesCollection = firestore.collection("messages");
+const messagesQuery = messagesCollection
   .orderBy("createdAt", "desc")
   .limit(100);
 
 export function useChat() {
   const messages = ref([]);
   const unsubscribe = messagesQuery.onSnapshot((snapshot) => {
-    messages.value = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    messages.value = snapshot.docs
+      .map((doc) => ({ id: doc.id, ...doc.data() }))
+      .reverse();
   });
-
   onUnmounted(unsubscribe);
-  const { user, isLogin } = useAuth();
 
+  const { user, isLogin } = useAuth();
   const sendMessage = (text) => {
     if (!isLogin.value) return;
-    const { photoURL, displayName, uid } = user.value;
-    messagesColleciton.add({
-      text,
+    const { photoURL, uid, displayName } = user.value;
+    messagesCollection.add({
+      userName: displayName,
+      userId: uid,
+      userPhotoURL: photoURL,
+      text: text,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      photoURL,
-      displayName,
-      uid,
     });
   };
 
