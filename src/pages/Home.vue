bfb1195709624c70b3f0ca5e5916c53b5f32fd63
CarraP <pablocarrassoumet@gmail.com>
2023-03-16 04:17:14 +0100

    feat(NotLoginComponent): Ajout de la page quand non connecté

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,16 @@
 <script setup>
   import Chat from '@/components/Chat.vue'
+  import NotLoggin from '@/components/NotLoggin.vue'
   import {useAuth} from '../firebase'
+
+  const {isLogin} = useAuth();
+
+
 </script>
 
 <template>
-  <Chat class="grow"/>
+  <Chat v-if="isLogin" class="grow"/>
+  <NotLoggin v-else/>
 </template>
 
 <style scoped>
